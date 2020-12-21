0b9eaa5bbaf8dfc5f76580409faf51c30410b114
kosovmax <kosovmax@ukr.net>
2020-12-21 11:58:46 +0200

    update

--- src/services/hits-api.js
+++ src/services/hits-api.js
@@ -1,13 +1,20 @@
 import axios from 'axios';
 
-const API_KEY = '19319242-70903095163a85f904f3acecb';
-
-// axios.defaults.headers.common['Authorization'] = 'Bearer 19319242-70903095163a85f904f3acecb';
+axios.defaults.baseURL = 'https://pixabay.com/api'
+axios.defaults.params = {}
+axios.defaults.params['key'] = '19319242-70903095163a85f904f3acecb'
 
 const fetchHits = ({ searchQuery = '',  page = 1, perPage = 12}) =>{
 
     return axios
-        .get(`https://pixabay.com/api/?q=${searchQuery}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=${perPage}`)
+        .get('/',{params:{
+            q:searchQuery,
+            page:page,
+            per_page:perPage,
+            image_type:'photo',
+            orientation:'horizontal'
+        }})
+        // .get(`https://pixabay.com/api/?q=${searchQuery}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=${perPage}`)
         .then(response => response.data.hits)
 
 }
